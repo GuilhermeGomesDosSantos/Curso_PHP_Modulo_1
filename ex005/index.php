44056fa400c56e0f8f7dff10758c4d5e06bf34f4
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-31 17:46:37 -0300

    Funções Aritméticas do PHP

---
+++ ex005/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+//abs, retorna o valor absoluto
+//$r = abs(500);
+// $r = abs(-5000);
+
+//base_convert() realiza a conversão
+// $r = base_convert(254, 10, 8);
+// $r = base_convert(254, 10, 16);
+// $r = base_convert(254, 10, 2);
+// $r = base_convert(1011, 2, 10);
+/*Funções de arredondamento
+ceil() arredonda para cima
+flor() arredonda para baixo
+round() round() arrendondamento de aritmetca
+*/
+// hypot() calcula o tamanho da hipotenusa
+//intdiv() retorna a divisão inteira
+// min() mostra o valor minimo, max() mostra o valor máximo
+// $r = min(5,8,2,1,6,8,2,4);
+// $r = max(5,8,2,1,6,8,2,4);
+//  $r = pi(); // usa o valor de PI
+// $r = M_PI; //usa o valor de PI
+// $r = pow(5,2); // cria potencia
+// sin(), cos(), tan() são utilizadas em trigonometria
+$r = sqrt(81); // calculo a raiz quadrada
+
+
+echo "O resultado é $r"
+?>
