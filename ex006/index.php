4510230c27f5d5e8e2bad22cad88323bdf1893df
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-31 19:53:41 -0300

    Operadores Aritméticos do PHP

---
+++ ex006/index.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Operador de concatenação é: . ponto
+Operador aritmético de adição é: +
+*/
+
+// $r = "2" + "2";
+// var_dump($r);
+
+// $r = "Gui" + "gds";
+// echo "$r";
+
+$r = "10" + "gui";
+echo "$r"
+?>
